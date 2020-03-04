3efc5f96633911167164271a5955c066280f00e4
kartikbhati29 <50818964+kartikbhati29@users.noreply.github.com>
2020-03-04 17:18:07 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,17 @@
+
+
+// filename Main.java 
+class Point { 
+	protected int x, y; 
+
+	public Point() { 
+	 
+	} 
+} 
+
+public class practice { 
+	public static void main(String args[]) { 
+	Point p = new Point(); 
+	System.out.println("x = " + p.x + ", y = " + p.y); 
+	}	 
+} 
